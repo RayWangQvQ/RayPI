a99e36c6d6865cdf39bde6b9e01be153ac50b9db
RayWang <2271272653@qq.com>
2020-06-11 18:32:42 +0800

    为EF Core添加全局过滤，使查询时自动过滤掉逻辑删除的数据

--- samples/RayPI.Repository.EFRepository/DbMapping/EntityBaseTypeConfig.cs
+++ samples/RayPI.Repository.EFRepository/DbMapping/EntityBaseTypeConfig.cs
@@ -5,6 +5,8 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using Ray.Infrastructure.Repository.EfCore;
+using System.Linq.Expressions;
+using Ray.Infrastructure.Auditing.Deletion;
 
 namespace RayPI.Repository.EFRepository.DbMapping
 {
@@ -21,18 +23,34 @@ namespace RayPI.Repository.EFRepository.DbMapping
             MyConfigureField(builder);
 
             builder.Property(x => x.CreatorId).IsRequired(false);
-            //builder.Property(x => x.CreateName).HasMaxLength(128).IsRequired(false);
             builder.Property(x => x.CreationTime).IsRequired();
 
             builder.Property(x => x.LastModifierId).IsRequired(false);
-            //builder.Property(x => x.UpdateName).HasMaxLength(128).IsRequired(false);
             builder.Property(x => x.LastModificationTime).IsRequired(false);
 
             builder.Property(x => x.IsDeleted).IsRequired();
             builder.Property(x => x.DeleterId).IsRequired(false);
             builder.Property(x => x.DeletionTime).IsRequired(false);
+
+            var filterExpression = CreateFilterExpression<TEntity>();
+            builder.HasQueryFilter(filterExpression);
         }
 
         public abstract void MyConfigureField(EntityTypeBuilder<TEntity> builder);
+
+        protected virtual Expression<Func<TEntity, bool>> CreateFilterExpression<TEntity>()
+            where TEntity : class
+        {
+            Expression<Func<TEntity, bool>> expression = null;
+
+            if (typeof(ISoftDelete).IsAssignableFrom(typeof(TEntity)))
+            {
+                expression = e => !EF.Property<bool>(e, "IsDeleted");
+            }
+
+            return expression;
+        }
     }
+
+
 }
