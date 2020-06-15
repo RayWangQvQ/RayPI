9002b8ab21c07dc827e24902ee536371c0a25c2b
WangRui <2271272653@qq.com>
2020-06-15 23:05:03 +0800

    封装AppService

---
+++ src/Ray.AppService/IAppServices/ICrudAppService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ray.AppService.IAppServices
+{
+    public interface ICrudAppService<TGetOutputDto, TGetListOutputDto, in TKey, in TGetListInput, in TCreateInput, in TUpdateInput>
+        : IQueryAppService<TGetOutputDto, TGetListOutputDto, TKey, TGetListInput>,
+        ICmdAppService<TGetOutputDto, TKey, TCreateInput, TUpdateInput>
+    {
+    }
+}
---
+++ src/Ray.AppService/IAppServices/IQueryAppService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Ray.Infrastructure.Helpers.Page;
+
+namespace Ray.AppService.IAppServices
+{
+    public interface IQueryAppService<TGetOutputDto, TGetListOutputDto, in TKey, in TGetListInput>
+    {
+        Task<TGetOutputDto> GetAsync(TKey id);
+
+        Task<PageResultDto<TGetListOutputDto>> GetListAsync(TGetListInput input);
+    }
+}
