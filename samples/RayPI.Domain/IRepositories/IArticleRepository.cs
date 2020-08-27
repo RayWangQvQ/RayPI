40b8719f6c60f639e44e35968a9d974dac4ef1cd
RayWang <2271272653@qq.com>
2020-08-27 21:21:27 +0800

    update

---
+++ samples/RayPI.Domain/IRepositories/IArticleRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Ray.Domain.Repositories;
+using RayPI.Domain.Entity;
+
+namespace RayPI.Domain.IRepositories
+{
+    public interface IArticleRepository:IBaseRepository<Article, Guid>
+    {
+    }
+}
